eaa7107b640c809fbcaff3cd2373da9fcab40d5f
Paul Calot <paul.calot@hotmail.fr>
2024-02-21 18:57:45 +0100

    Remove from module paths unused core models

--- src/medsamlaptop/models/core/__init__.py
+++ src/medsamlaptop/models/core/__init__.py
@@ -9,12 +9,11 @@
 
 # # Added for Edge SAM
 # from .rep_vit import *
-from .sam import Sam
-from .image_encoder import ImageEncoderViT
+# from .sam import Sam
+# from .image_encoder import ImageEncoderViT
 from .mask_decoder import MaskDecoder
 from .prompt_encoder import PromptEncoder
 from .transformer import TwoWayTransformer
-
 from .tiny_vit_sam import TinyViT
 
 # Added for Edge SAM
