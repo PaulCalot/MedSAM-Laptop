3a32d74a557f10b5998ab1f9971467a1399bc176
Paul Calot <paul.calot@hotmail.fr>
2024-02-25 20:42:32 +0100

    add compatiblity for both model

--- src/medsamlaptop/scripts/train_one_gpu.py
+++ src/medsamlaptop/scripts/train_one_gpu.py
@@ -78,6 +78,10 @@ parser.add_argument(
     "--sanity_check", action="store_true",
     help="Whether to do sanity check for dataloading."
 )
+parser.add_argument(
+    "--model_type", default="medSAMLite",
+    help="Type of backbone model : edgeSAM, medSAMLite. Default: medSAMLite"
+)
 
 args = parser.parse_args()
 args.work_dir = user.get_path_to_results() / args.work_dir
@@ -104,10 +108,12 @@ if args.sanity_check:
     print("SANITY CHECK...")
     script_utils.checks.perform_dataset_sanity_check(args.data_root)
 
-model_factory = medsamlaptop_models.MedEdgeSAMFactory()
-dataset_factory = medsamlaptop_data.Npy1024Factory(args.data_root)
-# model_factory = medsamlaptop_models.MedSAMLiteFactory()
-# dataset_factory = medsamlaptop_data.Npy256Factory(args.data_root)
+if(args.model_type=="edgeSAM"):
+    model_factory = medsamlaptop_models.MedEdgeSAMFactory()
+    dataset_factory = medsamlaptop_data.Npy1024Factory(args.data_root)
+elif(args.model_type=="medSAMLite"):
+    model_factory = medsamlaptop_models.MedSAMLiteFactory()
+    dataset_factory = medsamlaptop_data.Npy256Factory(args.data_root)
 facade = medsamlaptop_facade.SegmentAnythingPipeFacade(
                 model_factory
                 , dataset_factory)
@@ -134,9 +140,6 @@ lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
 )
 
 train_dataset = facade.get_dataset()
-# train_dataset = medsamlaptop_data.NpyDataset(data_root=args.data_root
-#                                              , data_aug=True
-#                                              , image_size=256) # depends on the model
 train_loader = torch.utils.data.DataLoader(train_dataset
                                 , batch_size=args.batch_size
                                 , shuffle=True
