246f483b51971ea0d4baaedb9bff90e500bfb805
Paul Calot <paul.calot@hotmail.fr>
2024-02-28 10:49:09 +0100

    Add new tests (not passing for now)

--- tests/datasets.py
+++ tests/datasets.py
@@ -24,5 +24,23 @@ class TestNpyDataset(unittest.TestCase):
             self.assertEqual(output['new_size'].shape, (2, ), "Shape of 'new_size' is incorrect")
             self.assertEqual(output['original_size'].shape, (2, ), "Shape of 'original_size' is incorrect")
 
+class TestEncoderDistillationDataset(unittest.TestCase):
+    data_root = pathlib.Path(pathlib.Path(__file__).resolve() / "../ref_data/npy/CT_Abd").resolve()
+    dataset_factory = medsamlaptop_data.Distillation1024Factory(path_to_data=data_root)
+    dataset = dataset_factory.create_dataset()
+    def test_output_shape(self):
+        for idx in range(len(self.dataset)):
+            expected_keys = ["image", "encoder_gts", "image_name", "new_size", "original_size"]
+            output = self.dataset[idx]
+            self.assertIsInstance(output, dict, "Output of Distillaiton dataset should be a dictionnary")
+            keys = list(output.keys())
+            for key in expected_keys:
+                self.assertIn(key, keys, f"{key} should be in {keys}")
+            self.assertEqual(output['image'].shape, (3, 1024, 1024), "Shape of 'image' is incorrect")
+            self.assertEqual(output['encoder_gts'].shape, (256, 64, 64), "Shape of 'gt2D' is incorrect")
+            self.assertIsInstance(output['image_name'], str, "Type of 'image_name' should be a string")
+            self.assertEqual(output['new_size'].shape, (2, ), "Shape of 'new_size' is incorrect")
+            self.assertEqual(output['original_size'].shape, (2, ), "Shape of 'original_size' is incorrect")
+
 if __name__ == '__main__':
     unittest.main()
