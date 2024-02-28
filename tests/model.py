fb16b0127936574c177563231d9847d65d61e239
Paul Calot <paul.calot@hotmail.fr>
2024-02-28 09:57:06 +0100

    Add test for MedSAM ; reduce batch size (pc was crashing)

--- tests/model.py
+++ tests/model.py
@@ -4,15 +4,14 @@ from medsamlaptop import models as medsamlaptop_models
 import torch
 
 class TestLittleMedSAM(unittest.TestCase):
-    batch_size = 8
+    batch_size = 2
     image_size = 256
     input_image = torch.rand(batch_size, 3, image_size, image_size) # (B, 3, 256, 256)
     input_box = torch.rand(batch_size, 1, 4) # (B, 1, 4)
     expected_low_res_masks_size = torch.Size([batch_size, 1, image_size, image_size])
     expected_iou_predictions_size = torch.Size([batch_size, 1])
     factory = medsamlaptop_models.MedSAMLiteFactory()
-    facade = medsamlaptop_models.SegmentAnythingModelFacade(factory)
-    model = facade.get_model()
+    model = factory.create_model()
     def test_output_shape(self):
         low_res_masks, iou_predictions = self.model(self.input_image, self.input_box)        
         self.assertEqual(self.expected_low_res_masks_size
@@ -24,7 +23,7 @@ class TestLittleMedSAM(unittest.TestCase):
 
 class TestEdgeSAM(unittest.TestCase):
     # NOTE: requires, in inputs, image of size 1024 !!
-    batch_size = 8
+    batch_size = 2
     image_size = 1024
     output_res = 256
     input_image = torch.rand(batch_size, 3, image_size, image_size) # (B, 3, 256, 256)
@@ -32,8 +31,28 @@ class TestEdgeSAM(unittest.TestCase):
     expected_low_res_masks_size = torch.Size([batch_size, 1, output_res, output_res])
     expected_iou_predictions_size = torch.Size([batch_size, 1])
     factory = medsamlaptop_models.MedEdgeSAMFactory()
-    facade = medsamlaptop_models.SegmentAnythingModelFacade(factory)
-    model = facade.get_model()
+    model = factory.create_model()
+    def test_output_shape(self):
+        low_res_masks, iou_predictions = self.model(self.input_image, self.input_box)        
+        self.assertEqual(self.expected_low_res_masks_size
+                         , low_res_masks.shape
+                         , "Low resolution mask shape error")
+        self.assertEqual(self.expected_iou_predictions_size
+                         , iou_predictions.shape
+                         , "IoU shape error")
+
+class TestMedSAM(unittest.TestCase):
+    # NOTE: requires, in inputs, image of size 1024 !!
+    batch_size = 2
+    image_size = 1024
+    output_res = 256
+    input_image = torch.rand(batch_size, 3, image_size, image_size) # (B, 3, 256, 256)
+    input_box = torch.rand(batch_size, 1, 4) # (B, 1, 4)
+    expected_low_res_masks_size = torch.Size([batch_size, 1, output_res, output_res])
+    expected_iou_predictions_size = torch.Size([batch_size, 1])
+    factory = medsamlaptop_models.MedSAMFactory()
+    model = factory.create_model()
+
     def test_output_shape(self):
         low_res_masks, iou_predictions = self.model(self.input_image, self.input_box)        
         self.assertEqual(self.expected_low_res_masks_size
