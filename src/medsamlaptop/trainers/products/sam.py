b8d1f559cdd1ac364211de1c1ead691d8e2a9b39
Paul Calot <paul.calot@hotmail.fr>
2024-03-02 21:52:04 +0100

    Remove useless comments

--- src/medsamlaptop/trainers/products/sam.py
+++ src/medsamlaptop/trainers/products/sam.py
@@ -2,14 +2,12 @@ from .interface import BaseTrainer
 
 class SamTrainer(BaseTrainer):
     def handle_batch(self, batch):
-        # Extract and send data to the device (GPU/CPU)
         image = batch["image"].to(self.device)
         gt2D = batch["gt2D"].to(self.device)
         boxes = batch["bboxes"].to(self.device)
-        return (image, boxes), gt2D  # Corresponding to inputs and targets respectively
+        return (image, boxes), gt2D
 
     def compute_loss(self, outputs, targets):
-        # Compute and return the loss
         logits_pred, iou_pred = outputs
         loss = self.loss_fn((logits_pred, iou_pred), targets)
         return loss
