bef0531f28d743734a118cb64ec75b70a0251140
Paul Calot <paul.calot@hotmail.fr>
2024-03-03 11:04:58 +0100

    Add inference run type

--- src/medsamlaptop/constants.py
+++ src/medsamlaptop/constants.py
@@ -3,8 +3,11 @@ EDGE_SAM_NAME = "edgeSAM"
 MED_SAM_LITE_NAME = "medSAMLite"
 MED_SAM_NAME = "MedSAM"
 
-
-# Run type
+# train Run type
 TRAIN_RUN_TYPE = "train"
 ENCODER_DISTILLATION_RUN_TYPE = "encoder-distillation"
 EDGE_SAM_STAGE_2_DISTILLATION_RUN_TYPE = "edgeSAM-stage2-distillation"
+
+# infer run type
+FULL_INFERENCE_RUN_TYPE = "full-inference"
+ENCODER_INFERENCE_RUN_TYPE = "encoder-inference"
