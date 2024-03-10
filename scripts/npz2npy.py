55512ea2c28b0a74bb7541090acb57c547d25eab
Paul Calot <paul.calot@hotmail.fr>
2024-03-10 20:41:44 +0100

    add script to handle all datasets at once

---
+++ scripts/npz2npy.py
@@ -0,0 +1,206 @@
+import glob
+from os import makedirs
+from os.path import join, basename
+from tqdm import tqdm
+import numpy as np
+import cv2
+import multiprocessing as mp
+from tqdm import tqdm
+
+from medsamtools import user
+from utils.name_mapper import (
+    NameMapper
+    , get_handler
+)
+
+def resize_longest_side(image, target_length=256):
+    """
+    Expects a numpy array with shape HxWxC in uint8 format.
+    """
+    long_side_length = target_length
+    oldh, oldw = image.shape[0], image.shape[1]
+    scale = long_side_length * 1.0 / max(oldh, oldw)
+    newh, neww = oldh * scale, oldw * scale
+    neww, newh = int(neww + 0.5), int(newh + 0.5)
+    target_size = (neww, newh)
+    
+    return cv2.resize(image, target_size, interpolation=cv2.INTER_AREA)
+
+
+def pad_image(image, target_length=256):
+    """
+    Expects a numpy array with shape HxWxC in uint8 format.
+    """
+    # Pad
+    h, w = image.shape[0], image.shape[1]
+    padh = target_length - h
+    padw = target_length - w
+    if len(image.shape) == 3: ## Pad image
+        image_padded = np.pad(image, ((0, padh), (0, padw), (0, 0)))
+    else: ## Pad gt mask
+        image_padded = np.pad(image, ((0, padh), (0, padw)))
+    
+    return image_padded
+
+
+def convert_npz_to_npy(input_):
+    """
+    Convert npz files to npy files for training
+
+    Parameters
+    ----------
+    npz_path : str
+        Name of the npz file to be converted
+    """
+    #name = npz_path.split(".npz")[0]
+    npz_path, npy_root_path, new_name = input_
+    try:
+        name = basename(npz_path).split(".npz")[0]
+        npz = np.load(npz_path, allow_pickle=True, mmap_mode="r")
+        imgs = npz["imgs"]
+        gts = npz["gts"]
+        img_root_saving_dir = npy_root_path / "imgs"
+        gts_root_saving_dir = npy_root_path / "gts"
+        img_root_saving_dir.mkdir(parents=True, exist_ok=True)
+        gts_root_saving_dir.mkdir(parents=True, exist_ok=True)
+        if len(gts.shape) > 2: ## 3D image
+            for i in range(imgs.shape[0]):
+                img_i = imgs[i, :, :]
+                gt_i = gts[i, :, :]
+                if do_resize_256:
+                    img_i = resize_longest_side(img_i)
+                    gt_i = cv2.resize(gt_i, (img_i.shape[1], img_i.shape[0]), interpolation=cv2.INTER_NEAREST)
+                    img_i = pad_image(img_i)
+                    gt_i = pad_image(gt_i)
+
+                img_3c = np.repeat(img_i[:, :, None], 3, axis=-1)
+
+                img_01 = (img_3c - img_3c.min()) / np.clip(
+                    img_3c.max() - img_3c.min(), a_min=1e-8, a_max=None
+                )  # normalize to [0, 1], (H, W, 3)
+
+                gt_i = gts[i, :, :]
+
+                gt_i = np.uint8(gt_i)
+                assert img_01.shape[:2] == gt_i.shape
+
+                np.save(img_root_saving_dir / f"{new_name}_{i:03d}.npy", img_01)
+                np.save(gts_root_saving_dir / f"{new_name}_{i:03d}.npy", gt_i)
+        else: ## 2D image
+            if len(imgs.shape) < 3:
+                img_3c = np.repeat(imgs[:, :, None], 3, axis=-1)
+            else:
+                img_3c = imgs
+
+            if do_resize_256:
+                img_3c = resize_longest_side(img_3c)
+                #gts = resize_longest_side(gts)
+                gts = cv2.resize(gts, (img_3c.shape[1], img_3c.shape[0]), interpolation=cv2.INTER_NEAREST)
+                img_3c = pad_image(img_3c)
+                gts = pad_image(gts)
+
+            img_01 = (img_3c - img_3c.min()) / np.clip(
+                img_3c.max() - img_3c.min(), a_min=1e-8, a_max=None
+            )  # normalize to [0, 1], (H, W, 3)
+            assert img_01.shape[:2] == gts.shape
+            np.save(img_root_saving_dir / f"{new_name}.npy", img_01)
+            np.save(gts_root_saving_dir / f"{new_name}.npy", gts)
+            # np.save(join(npy_dir, "imgs", name + ".npy"), img_01)
+            # np.save(join(npy_dir, "gts", name + ".npy"), gts)
+    except Exception as e:
+        print(e)
+        print(npz_path)
+
+# Target : 
+# Pivot format <dataset a-zA-Z0-9>_<modality>_<anatomy>_<id> 
+# with id relative to the dataset (for now)
+# we need converter for each dataset 
+# What should be the image ID, should it be unique ?
+# What size should it be given ?
+# we will create a unique ID of image, which means we will allow up to 1M images
+# and will create a mapper : old name => new name
+# or rather: new unique id => dataset, id etc.
+# There should be some way to make sure this is repeatalbe (that is id are unique throughout each system)
+# what would it allow us to do, that to have unique ID ?
+
+# modality id : 4 0-9
+
+datasets_paths = {
+    "hc18": "US/hc18"
+    , "Breast-Ultrasound": "US/Breast-Ultrasound"
+    , "autoPET": "PET/autoPET"
+    , "Intraretinal-Cystoid-Fluid": "OCT/Intraretinal-Cystoid-Fluid"
+    , "NeurIPS22CellSeg": "Microscopy/NeurIPS22CellSeg"
+    , "m2caiSeg": "Endoscopy/m2caiSeg"
+    , "Kvasir-SEG": "Endoscopy/Kvasir-SEG"
+    , "CholecSeg8k": "Endoscopy/CholecSeg8k"
+    , "CT_AbdTumor": "CT/CT_AbdTumor"
+    , "AbdomenCT1K": "CT/AbdomenCT1K"
+    , "AMOD": "CT/AMOS"
+}
+
+possibilities_CholecSeg_8K = [
+    "AbdominalWall"
+    , "Blood"
+    , "ConnectiveTissue"
+    , "CysticDuct"
+    , "Fat"
+    , "Gallbladder"
+    , "GastrointestinalTract"
+    , "Grasper"
+    , "HepaticVein"
+    , "LhookElectrocautery"
+    , "Liver"
+]
+
+possibilities_CT_AbdTmor = [
+        "Adrenal"
+        , "case" # this one is weird => is it another kind of format ???
+        , "colon" 
+        , "hepaticvessel"
+        , "liver"
+        , "pancreas"
+        , "PETCT" # this one is weird too
+]
+
+# To use the NameMapper, we need to create, for each data point, the dictionnaru with various inputs
+# for now, we will not use any mapper to associate to an modality an id
+# nor will we convert the images
+# we just want to generate the csv
+
+root_path = user.get_path_to_data()
+mapper_name = "MAPPER"
+root_saving_directory = root_path / "PROCESSED"
+if __name__ == "__main__":
+    inputs = []
+    with NameMapper(root_path / f"{mapper_name}.csv") as mapper:
+        for name_dataset, rel_path in datasets_paths.items():
+            print(f"Processing {name_dataset}...")
+            path_to_dataset = root_path / rel_path
+            lst_paths = sorted(path_to_dataset.glob("*.npz"))
+            dataset_handler = get_handler(name_dataset)
+            for k, path_ in tqdm(enumerate(lst_paths)):
+                try:
+                    (status, dico) = dataset_handler(path_.stem)
+                except Exception as e:
+                    print(f"[{k}] Exception: {name_dataset} - {path_.stem} - {e}")
+                    continue
+                if(status == "KO"):
+                    print(f"[{k}] FAIL: {name_dataset} - {path_.stem} (reason: {dico})")
+                else:
+                    new_name = mapper.get_new_name(**dico)
+                inputs.append( # npz_path, npy_root_path, new_name
+                    (path_, root_saving_directory / name_dataset, new_name)
+                )
+    # npz_dir = "train_npz"
+    # npy_dir = "train_npy"
+    num_workers = 8
+    do_resize_256 = False # whether to resize images and masks to 256x256
+    # makedirs(npy_dir, exist_ok=True)
+    # makedirs(join(npy_dir, "imgs"), exist_ok=True)
+    # makedirs(join(npy_dir, "gts"), exist_ok=True)
+    # npz_paths = glob.glob(join(npz_dir, "**/*.npz"), recursive=True)
+    with mp.Pool(num_workers) as p:
+        r = list(tqdm(p.imap(convert_npz_to_npy, inputs), total=len(inputs)))
+    
+            
---
+++ scripts/utils/name_mapper.py
@@ -0,0 +1,275 @@
+import re
+import pandas as pd
+import pathlib
+from typing import Optional, Dict, Tuple
+
+class NameMapper:
+    # TODO: add getter for old name etc?
+    # TODO: what of race conditions ?
+    PK_COLUMNS = ["dataset", "old_name"]
+    EXPECTED_COLUMNS = ["primary_key", "dataset", "modality", "anatomy", "target_task", "old_name", "new_name", "id"]
+
+    def __init__(self
+                 , path_to_mapper: pathlib.Path):
+        assert path_to_mapper.suffix == ".csv", "Input path should be a .csv file" 
+        self.path_to_mapper = path_to_mapper
+        if(not self.path_to_mapper.is_file()):
+            df_mapper = pd.DataFrame(columns=self.EXPECTED_COLUMNS)
+        else:
+            df_mapper = pd.read_csv(self.path_to_mapper)
+        assert(all([ col in df_mapper.columns for col in self.EXPECTED_COLUMNS]))
+        self.df_mapper = df_mapper
+
+    def get_new_name(self
+                , dataset: str
+                , old_name: str
+                , modality: Optional[str] = 'UNK'
+                , anatomy: Optional[str] = 'UNK'
+                , target_task: Optional[str] = 'UNK') -> str:
+        # TODO: should we check for dataset /  old_name validity ?
+        dataset = remove_non_alphanumeric(dataset).lower()
+        old_name_formatted = remove_non_alphanumeric(old_name).lower()
+        primary_key = f"{dataset}_{old_name_formatted}"
+        if(primary_key in self.df_mapper["primary_key"].values):
+            # NOTE in theory there should be only one
+            new_name = self.df_mapper[self.df_mapper["primary_key"] == primary_key]["new_name"].iloc[0]
+            return new_name
+
+        new_id = self.generate_new_id()
+        new_name = f"{dataset}_{modality}_{anatomy}_{new_id}"
+        new_row = {
+            "primary_key": primary_key
+            , "dataset": dataset
+            , "modality": modality
+            , "anatomy": anatomy
+            , "target_task": target_task
+            , "old_name": old_name
+            , "new_name": new_name
+            , "id": new_id 
+        }
+        self.df_mapper.loc[self.df_mapper.shape[0]] = new_row
+        return new_name
+
+    def generate_new_id(self) -> str:
+        # works like an auto-increment on database
+        return "{:07d}".format(len(self.df_mapper)+1) # starts at 1
+
+    def _save(self):
+        self.df_mapper.to_csv(self.path_to_mapper)
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._save()
+        assert self.df_mapper["id"].nunique() == len(self.df_mapper), "ID is not unique (Race condition ?)"
+
+def get_handler(dataset):
+    dataset_to_authorized_pattern = {
+        "hc18": handle_hc18
+        , "Breast-Ultrasound": handle_breastultrasound
+        , "autoPET": handle_autopet
+        , "Intraretinal-Cystoid-Fluid": handle_intraretinalcystoidfluid
+        , "NeurIPS22CellSeg": handle_neurips22cellseg
+        , "m2caiSeg": handle_m2caiseg
+        , "Kvasir-SEG": handle_kvasirseg
+        , "CholecSeg8k": handle_cholecseg8k
+        , "CT_AbdTumor": handle_abdtumor
+        , "AbdomenCT1K": handle_abdomenct1K
+        , "AMOD": handle_amod
+    }
+    return dataset_to_authorized_pattern[dataset]
+
+# TODO: clean that
+# ---------------- UTILS 
+dataset_to_authorized_pattern = {
+    "hc18": [r"US_hc18_(?P<NUM>[0-9]{3})_(?P<RANK>[0-9]{0,1})HC"]
+    , "Breast-Ultrasound": [r"US_Breast-Ultrasound_(?P<TYPE>benign|malignant|normal) \((?P<NUM>[0-9]+\))"]
+    , "autoPET": [r"PET_Lesion_PETCT_(?P<ID>[a-zA-Z0-9]{10})"]
+    , "Intraretinal-Cystoid-Fluid": [r"OCT_Intraretinal-Cystoid-Fluid_(?P<SOMETHING>\w)"]
+    , "NeurIPS22CellSeg": [r"Microscopy_NeurIPS22CellSeg_cell_(?P<ID>[0-9]{5})"]
+    , "m2caiSeg": [r"Endoscopy_m2caiSeg_(?P<ID>[0-9]{3})_(?P<FRAME>[0-9]+)"] # this is an hypothesis... from the da
+    , "Kvasir-SEG": [r"Endoscopy_Kvasir-SEG_(?P<ID>[a-z0-9]{25})"]
+    # TODO : we don't enforce from there the possiblitiees that we saw - we should do it afterwards
+    , "CholecSeg8k": [r"Endoscopy_CholecSeg8k_(?P<ORGAN>[a-zA-Z]*)_video(?P<ID>[0-9]+)_frame_(?P<FRAME>[0-9]+)"]
+    # TODO: once again, we are kind of lazy, in practice this dataset is already a merge without clean pivotal format
+    , "CT_AbdTumor": [r"CT_AbdTumor_(?P<ORGAN>[a-zA-Z]+)_(?P<ID>[a-zA-Z0-9]+)"]
+    , "AbdomenCT1K": [r"CT_AbdomenCT_Case_(?P<ID>[0-9]{5})"]
+    , "AMOD": [r"CT_AMOS_amos_(?P<ID>[0-9]{4})"]
+}
+
+def handle_amod(name: str):
+    patterns = dataset_to_authorized_pattern["AMOD"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "AMOD"
+                    , "old_name": name
+                    , "modality": "CT"
+                    # TODO
+                    # , "anatomy": ""
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_abdomenct1K(name: str):
+    patterns = dataset_to_authorized_pattern["AbdomenCT1K"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "AbdomenCT1K"
+                    , "old_name": name
+                    , "modality": "CT"
+                    # TODO
+                    # , "anatomy": ""
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_abdtumor(name: str):
+    patterns = dataset_to_authorized_pattern["CT_AbdTumor"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "AbdTumor"
+                    , "old_name": name
+                    , "modality": "CT"
+                    # TODO
+                    # , "anatomy": ""
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_cholecseg8k(name: str):
+    patterns = dataset_to_authorized_pattern["CholecSeg8k"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "CholecSeg8k"
+                    , "old_name": name
+                    , "modality": "Endoscopy"
+                    # TODO
+                    # , "anatomy": ""
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_kvasirseg(name: str):
+    patterns = dataset_to_authorized_pattern["Kvasir-SEG"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "Kvasir-SEG"
+                    , "old_name": name
+                    , "modality": "Endoscopy"
+                    # TODO
+                    # , "anatomy": ""
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_m2caiseg(name: str):
+    patterns = dataset_to_authorized_pattern["m2caiSeg"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "m2caiSeg"
+                    , "old_name": name
+                    , "modality": "Endoscopy"
+                    # TODO
+                    # , "anatomy": ""
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_neurips22cellseg(name: str):
+    patterns = dataset_to_authorized_pattern["NeurIPS22CellSeg"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "NeurIPS22CellSeg"
+                    , "old_name": name
+                    , "modality": "Microscopy"
+                    # TODO
+                    # , "anatomy": "retina"
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_intraretinalcystoidfluid(name: str):
+    patterns = dataset_to_authorized_pattern["Intraretinal-Cystoid-Fluid"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "Intraretinal-Cystoid-Fluid"
+                    , "old_name": name
+                    , "modality": "OCT" 
+                    , "anatomy": "retina"
+                    # , "target_task": ""
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_autopet(name: str):
+    patterns = dataset_to_authorized_pattern["autoPET"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "autoPET"
+                    , "old_name": name
+                    , "modality": "PET" # /CT" TODO: is this also CT ?
+                    , "target_task": "automated PET lesion segmentation"
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_breastultrasound(name: str) -> Tuple[str, Dict|str]:
+    patterns = dataset_to_authorized_pattern["Breast-Ultrasound"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            dico = match.groupdict()
+            breast_type = dico["TYPE"]
+            return ("OK", {
+                    "dataset": "Breast-Ultrasound"
+                    , "old_name": name
+                    , "modality": "US"
+                    , "anatomy": f"{breast_type} breast"
+                    , "target_task": "breast cancer detection"
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def handle_hc18(name: str):
+    patterns = dataset_to_authorized_pattern["hc18"]
+    for pattern in patterns:
+        match = re.match(pattern, name)
+        if(match):
+            # dico = match.groupdict()
+            return ("OK", {
+                    "dataset": "hc18"
+                    , "old_name": name
+                    , "modality": "US"
+                    , "anatomy": "fetus"
+                    , "target_task": "fetal head circumference"
+                    })
+    return ("KO", f"Name not recognized: {name}")
+
+def remove_non_alphanumeric(s):
+    return ''.join([char for char in s if char.isalnum()])
+
