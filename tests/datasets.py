18d7dca51b52c99a64512b90dcf640cb3f9e953c
Paul Calot <paul.calot@hotmail.fr>
2024-03-10 13:03:14 +0100

    Add test of fetcher

--- tests/datasets.py
+++ tests/datasets.py
@@ -1,8 +1,36 @@
 import unittest
 import pathlib
 import torch
+import json
 
 from medsamlaptop import datasets as medsamlaptop_datasets
+from medsamlaptop.datasets.products.fetcher import JsonDataParser
+
+class TestJsonDataParser(unittest.TestCase):
+    root_path_to_test_data = pathlib.Path(pathlib.Path(__file__).resolve() / "../ref_data").resolve()
+    path_to_json = root_path_to_test_data / "data_example.json"
+    with open(path_to_json, "r") as f:
+        json_data_file = json.load(f)
+    json_parser = JsonDataParser(json_data_file, root_path_to_test_data / "npy/")
+
+    def test_loading(self):
+        subfolders = ["gts", "encoder_gts", "imgs", "teacher_gts"]
+        paths = self.json_parser.get_paths(subfolders)
+        
+        expected_keys = ["CT_Abd_FLARE22_Tr_0001-000.npy"
+                         , "CT_Abd_FLARE22_Tr_0001-005.npy"
+                         , "CT_Abd_FLARE22_Tr_0001-010.npy"
+                         , "CT_Abd_FLARE22_Tr_0001-028.npy"]
+        for expected_key in expected_keys:
+            self.assertIn(expected_key, paths.keys())
+            dico = paths[expected_key]
+            self.assertTrue(all([subfolder in dico.keys() for subfolder in subfolders]))
+            for subfolder in subfolders:
+                path_value = dico[subfolder]
+                self.assertIsInstance(path_value, pathlib.Path)
+                self.assertTrue(path_value.is_file())
+                self.assertEqual(path_value
+                                 , self.root_path_to_test_data / f"npy/CT_Abd/{subfolder}/{expected_key}")
 
 class TestNpyDataset(unittest.TestCase):
     data_root = pathlib.Path(pathlib.Path(__file__).resolve() / "../ref_data/npy/CT_Abd").resolve()
