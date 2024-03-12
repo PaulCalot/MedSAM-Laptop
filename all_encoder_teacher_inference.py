6e5bad0aac63c032e26d26adcad4070f345cbf82
Paul Calot <paul.calot@hotmail.fr>
2024-03-12 16:54:39 +0100

    Add code to run all encoder teacher inferences for all datasets

---
+++ all_encoder_teacher_inference.py
@@ -0,0 +1,20 @@
+from medsamtools.user import get_path_to_data
+import os
+import tqdm
+
+command_pattern = """python scripts/teacher_inference.py \
+    --data_root {} \
+    --pretrained_checkpoint med-sam/medsam_vit_b.pth \
+    --model_type MedSAM \
+    --device cuda:0
+    --run_type encoder-inference
+"""
+
+root_dir = get_path_to_data()
+lst = (root_dir / "PROCESSED").glob("*")
+
+for dataset_directory_path in tqdm.tqdm(lst):
+    if(not dataset_directory_path.is_dir()):
+        continue
+    print(f"\n #-----------{dataset_directory_path}---------# \n")
+    os.system(command_pattern.format(dataset_directory_path))
