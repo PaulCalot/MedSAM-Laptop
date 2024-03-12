9486eac3d400f12446f085674dc7cce7f3acfa23
Paul Calot <paul.calot@hotmail.fr>
2024-03-12 17:33:18 +0100

    New version that should take all data (to be tested)

--- src/medsamlaptop/datasets/products/fetcher.py
+++ src/medsamlaptop/datasets/products/fetcher.py
@@ -1,20 +1,28 @@
 import pathlib
+import re
 import logging
 from typing import Dict, List, Tuple
-
 logger = logging.getLogger(__name__)
 # TODO : move this out 
+
+processed_root_dir = "PROCESSED/"
 id_to_root_path_mapper = {
-    # Exemple : "mfile odality<1>": "path-...-..."   
-    "FLARE22": "FLARE22Train/data/npy/CT_Abd/"
-    # Unittest
+    "hc18": "hc18"
+    , "breastultrasound": "Breast-Ultrasound" 
+    , "autopet": "autoPET"
+    , "intraretinalcystoidfluid": "Intraretinal-Cystoid-Fluid"
+    , "Neurips22cellseg": "NeurIPS22CellSeg"
+    , "m2caiseg": "m2caiSeg"
+    , "kvasirseg": "Kvasir-SEG" 
+    , "cholecseg8k": "CholecSeg8k"
+    , "ctabdtumor": "CT_AbdTumor"
+    , "abdomenct1k": "AbdomenCT1K"
+    , "amod": "AMOD"
+    # Unittest - to be handled differently - TODO: move this out
     , "unittest_CT": "CT_Abd"
 }
 
-# name should look like : <something with _ and a-zA-Z0-9>_XXXX-YYY.npy
-# the XXXX is the modality
-# TODO: this does not work because the first part also validates for modality_id
-NAME_PATTERN = r'(?P<root_name>\w)_(?P<modality_id>\d{4})-(?P<data_id>\d{3}).npy'
+NAME_PATTERN = r'(?P<dataset>[a-zA-Z0-9]+)_(?P<modality>[a-zA-Z0-9]+)_(?P<anatomy>[a-zA-Z0-9]+)_(?P<id>[a-zA-Z0-9]+)_'
 
 class IncorrectDataNameFormat(Exception):
     def __init__(self, name: str):
@@ -27,9 +35,7 @@ class JsonDataParser:
     def __init__(self
                  , json_data_file: Dict
                  , root_path: pathlib.Path) -> None:
-        assert("modality" in json_data_file.keys())
         assert("data" in json_data_file.keys())
-        self.modalities =  json_data_file["modality"]
         self.data = json_data_file["data"]
         self.root_dir = root_path
 
@@ -47,39 +53,34 @@ class JsonDataParser:
 
     def parse_name(self, name: str):
         try:
-            modality_id = self.try_modality_parser(name)
+            dataset_id = self.try_dataset_parser(name)
             return ("OK", {"name": name
-                           , "modality": modality_id})
+                           , "dataset": dataset_id})
         except IncorrectDataNameFormat as e:
             return ("KO", e)
         
     def make_paths_for_name(self, input: Tuple[str, Dict | Exception], subfolders: List[str]):
         status, info = input
         if(status == "KO" or isinstance(info, Exception)):
-            return input # ("KO", <error>)
+            return input
 
-        assert(all( [expected_key in info.keys() for expected_key in ("modality", "name")] ))
-        modality_id = info["modality"]
+        assert(all( [expected_key in info.keys() for expected_key in ("dataset", "name")] ))
+        dataset_id = info["dataset"]
         name = info["name"]
 
-        # NOTE: two steps, we suppose that the jsn contains the link between the IDs and some modality name
-        # then we go and fetch it using the reference
-        modality_name = self.modalities[modality_id]
-        modality_root_path = id_to_root_path_mapper[modality_name]
+        if(not dataset_id in id_to_root_path_mapper):
+            return ("KO", f"Missing dataset key in mapper: {dataset_id}")
+        dataset_name = id_to_root_path_mapper[dataset_id]
         paths = {}
         for subfolder in subfolders:
-            path = ((self.root_dir / modality_root_path) / subfolder) / name
+            path = (((self.root_dir / processed_root_dir) / dataset_name) / subfolder) / f"{name}.npy"
             if(not path.is_file()):
                 return ("KO", f"Data does not exist: {path}")
             paths[subfolder] = path
         return ("OK", paths)
 
-    def try_modality_parser(self, name:str):
-        # match = re.match(NAME_PATTERN, name)
-        # if(match):
-        #     return match.groupdict()["modality_id"]
-        if(len(name) >= 12):
-            # TODO: this is really really stupid
-            modality = name[-12: -8]
-            return modality
+    def try_dataset_parser(self, name:str):
+        match = re.match(NAME_PATTERN, name)
+        if(match):
+            return match.groupdict()["dataset"]
         raise IncorrectDataNameFormat(name)
