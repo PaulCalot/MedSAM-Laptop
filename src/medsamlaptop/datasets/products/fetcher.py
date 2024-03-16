100538435a4e77d715a4296949edd9200c647faa
Paul Calot <paul.calot@hotmail.fr>
2024-03-16 12:02:49 +0100

    Fix problem with dictionnary creation

--- src/medsamlaptop/datasets/products/fetcher.py
+++ src/medsamlaptop/datasets/products/fetcher.py
@@ -6,21 +6,67 @@ logger = logging.getLogger(__name__)
 # TODO : move this out 
 
 processed_root_dir = "PROCESSED/"
-id_to_root_path_mapper = {
-    "hc18": "hc18"
-    , "breastultrasound": "Breast-Ultrasound" 
-    , "autopet": "autoPET"
-    , "intraretinalcystoidfluid": "Intraretinal-Cystoid-Fluid"
-    , "Neurips22cellseg": "NeurIPS22CellSeg"
-    , "m2caiseg": "m2caiSeg"
-    , "kvasirseg": "Kvasir-SEG" 
-    , "cholecseg8k": "CholecSeg8k"
-    , "ctabdtumor": "CT_AbdTumor"
-    , "abdomenct1k": "AbdomenCT1K"
-    , "amod": "AMOD"
-    # Unittest - to be handled differently - TODO: move this out
-    , "unittest_CT": "CT_Abd"
+datasets_paths = {
+    "hc18": "US/hc18"
+    , "Breast-Ultrasound": "US/Breast-Ultrasound"
+    , "autoPET": "PET/autoPET"
+    , "Intraretinal-Cystoid-Fluid": "OCT/Intraretinal-Cystoid-Fluid"
+    , "NeurIPS22CellSeg": "Microscopy/NeurIPS22CellSeg"
+    , "m2caiSeg": "Endoscopy/m2caiSeg"
+    , "Kvasir-SEG": "Endoscopy/Kvasir-SEG"
+    , "CholecSeg8k": "Endoscopy/CholecSeg8k"
+    , "CT_AbdTumor": "CT/CT_AbdTumor"
+    , "AbdomenCT1K": "CT/AbdomenCT1K"
+    , "AMOD": "CT/AMOS"
+    , "ISIC2018": "Dermoscopy/ISIC2018"
+    , "IDRiD": "Fundus/IDRiD"
+    , "PAPILA": "Fundus/PAPILA"
+    , "CDD-CESM": "Mammography/CDD-CESM"
+    , "AMOSMR": "MR/AMOSMR"
+    , "BraTS_FLAIR": "MR/BraTS_FLAIR"
+    , "BraTS_T1": "MR/BraTS_T1"
+    , "BraTS_T1CE": "MR/BraTS_T1CE"
+    , "CervicalCancer": "MR/CervicalCancer"
+    , "crossmoda": "MR/crossmoda"
+    , "Heart": "MR/Heart"
+    , "ISLES2022_ADC": "MR/ISLES2022_ADC"
+    , "ISLES2022_DWI": "MR/ISLES2022_DWI"
+    , "ProstateADC": "MR/ProstateADC"
+    , "ProstateT2": "MR/ProstateT2"
+    , "QIN-PROSTATE-Lesion": "MR/QIN-PROSTATE-Lesion"
+    , "QIN-PROSTATE-Prostate": "MR/QIN-PROSTATE-Prostate"
+    , "SpineMR": "MR/SpineMR"
+    , "WMH_FLAIR": "MR/WMH_FLAIR"
+    , "WMH_T1": "MR/WMH_T1"
+    , "Chest-Xray-Masks-and-Labels": "XRay/Chest-Xray-Masks-and-Labels"
+    , "COVID-19-Radiography-Database": "XRay/COVID-19-Radiography-Database"
+    , "COVID-QU-Ex-lungMask_CovidInfection": "XRay/COVID-QU-Ex-lungMask_CovidInfection"
+    , "COVID-QU-Ex-lungMask_Lung": "XRay/COVID-QU-Ex-lungMask_Lung"
+    , "Pneumothorax-Masks": "XRay/Pneumothorax-Masks"
 }
+
+def remove_non_alphanumeric(s):
+    return ''.join([char for char in s if char.isalnum()])
+
+id_to_root_path_mapper = {}
+for key, index in datasets_paths.items():
+    id_to_root_path_mapper[remove_non_alphanumeric(key)] = key
+ 
+# id_to_root_path_mapper = {
+#     "hc18": "hc18"
+#     , "breastultrasound": "Breast-Ultrasound" 
+#     , "autopet": "autoPET"
+#     , "intraretinalcystoidfluid": "Intraretinal-Cystoid-Fluid"
+#     , "Neurips22cellseg": "NeurIPS22CellSeg"
+#     , "m2caiseg": "m2caiSeg"
+#     , "kvasirseg": "Kvasir-SEG" 
+#     , "cholecseg8k": "CholecSeg8k"
+#     , "ctabdtumor": "CT_AbdTumor"
+#     , "abdomenct1k": "AbdomenCT1K"
+#     , "amod": "AMOD"
+#     # Unittest - to be handled differently - TODO: move this out
+#     , "unittest_CT": "CT_Abd"
+# }
 
 NAME_PATTERN = r'(?P<dataset>[a-zA-Z0-9]+)_(?P<modality>[a-zA-Z0-9]+)_(?P<anatomy>[a-zA-Z0-9]+)_(?P<id>[a-zA-Z0-9]+)_'
 
